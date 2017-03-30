42ff3f227fbf6583f81d1612555bd92d83a6172b
Yiling <emily_chuang@htc.com>
2017-03-30 21:57:09 +0800

    initial commit

---
+++ app/src/main/java/com/eri/ericathay/SubmissionActivity.java
@@ -0,0 +1,16 @@
+package com.eri.ericathay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubmissionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+
+        //TODO: create some fragment classes
+
+    }
+}
